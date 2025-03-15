1454b6ffd30766f401a0f5dcf555b9fbd8a402e5
BulgakovDmitry <Bulgakov.di@phystech.edu>
2025-03-15 04:35:22 +0300

    create specifiers (now just replace)

--- myPrintf.s
+++ myPrintf.s
@@ -1,34 +1,97 @@
+    
     section .text
     global _start
+
 
     ;----------------------------------------------------------------------------------------------
     _start:
 
-        lea rsi, message                         ; rsi = &message
-        call myPuts   
-
+        lea rsi, messageForDisplaying            ; rsi = &message
+        call myPrintf
         call exit0
     ;----------------------------------------------------------------------------------------------
 
-
     ;----------------------------------------------------------------------------------------------
-    myPuts:    
+    myPrintf:
     ;   Function myPrintf is used to display text on the screen
-    ;   Entry:   rsi - message, that should be displayed on the screen
-    ;   Destr:   rax - number of system call sys_write
-    ;            rdi - value for "repne scasb" in function myStrlen - file descriptor for standard output (stdout)
-    ;            rdx - length of the line displayed on the screen
-    ;            rcx - ret value in function myStrlen
+    ;   Entry:   rsi - message, that should be displayed on the screen & address of buffer
+    ;   Destr:   rax - [destr myPutc]
+    ;            rbx - addess of buffer
+    ;            rdi - [entry myStrlen] & [destr myPutc]
+    ;            rdx - [destr myPutc]
+    ;            rcx - [ret myStrlen]
     ;   Ret:     None 
+
+        mov rbx, buffer                       ; create address of buffer (rbx)
+
         mov rdi, rsi                             ; rdi = rsi = &message
         call myStrlen                            ; rcx = strlen(message)
 
-        mov rdx, rcx                             ; rdx = strlen(Hello world) + strlen(\n) = 11 + 2 ; ------ 
+        call fillBuffer
+
+        mov rsi, buffer 
+    putc_loop:
+        push rcx                                 ; save rcx because of syscall 
+        call myPutc
+        inc rsi
+        pop rcx                                  ; save rcx because of syscall                                                                                         
+        loop putc_loop
+
+    ret
+    ;----------------------------------------------------------------------------------------------
+
+
+    ;----------------------------------------------------------------------------------------------
+    fillBuffer:
+    ;   
+    ;
+    ;
+        push rcx                                 ; save len of messageForDisplaying
+    fillBufferCycle:
+        mov al, '%'                              ; al = '%'
+        mov dl, [rsi]                            ; dl = *rsi = *(messageForDisplaying + i)
+        cmp dl, al                               ; if (al == dl)
+        je specifier                             ;      goto specifier
+        jne notSpecifier                         ; else goto notSpecifier
+
+    specifier:                                   ; ___specifier___
+        mov al, '!'
+        mov [rbx], al
+        inc rbx
+        inc rsi
+        jmp endOfCycle
+
+    notSpecifier:                                ; ___notSpecifier___
+        mov al, [rsi]                            ; al = *rsi = *(messageForDisplaying + i)
+        mov [rbx], al                            ; *(rbx) = *(buffer) = al = *rsi = *(messageForDisplaying + i)
+        inc rsi                                  ; rsi++
+        inc rbx                                  ; rbi++
+        jmp endOfCycle                           ; goto endOfCycle
+
+    endOfCycle:
+
+        loop fillBufferCycle                     ; while (cx--)
+        pop rcx                                  ; save len of messageForDisplaying
+
+    ret
+    ;----------------------------------------------------------------------------------------------
+
+
+    ;----------------------------------------------------------------------------------------------
+    myPutc:    
+    ;   Function myPutc used to display char on the screen
+    ;   Entry:   rsi - char address, that should be displayed on the screen
+    ;   Destr:   rax - number of system call sys_write
+    ;            rdi - file descriptor for standard output (stdout)
+    ;            rdx - length of the line displayed on the screen (1 for char)
+    ;   Ret:     None 
+
+        mov rdx, 1                               ; rdx = strlen([char]) = 1                        ; ------
         mov rax, 1                               ; sys_write                                       ; output
         mov rdi, 1                               ; stdout                                          ; to    
         syscall                                  ; system call                                     ; screen
 
-        ret
+    ret
     ;----------------------------------------------------------------------------------------------
 
 
@@ -47,10 +110,11 @@
         cld                                      ; rdi will increase
         repne scasb                              ; while (cx-- && al != rsi++)
 
-        neg rcx                                  ; rcx * (-1) [it is len + 1 at the moment]
-        dec rcx                                  ; rcx--      [it is len     at the moment]
+        neg rcx                                  ; rcx * (-1) [it is len + 2 at the moment]
+        sub rcx, 2                               ; rcx-= 2    [it is len     at the moment]
+        
 
-        ret
+    ret
     ;----------------------------------------------------------------------------------------------
 
    
@@ -64,16 +128,21 @@
         mov rax, 60                              ; sys_exit     ;
         xor rdi, rdi                             ; rdi = 0      ; exit(0)
         syscall                                  ; system call  ;
-        ret
+
+    ret
+    ;----------------------------------------------------------------------------------------------
+
+   
+    ;----------------------------------------------------------------------------------------------
+    section .data                                                                                 
+
+    messageForDisplaying:
+        db "Hello wo%rld", 10
     ;----------------------------------------------------------------------------------------------
 
 
     ;----------------------------------------------------------------------------------------------
-    section .data                        
+    section .bss
 
-    message:
-        db "Hello world", 10
+    buffer resb 100 ; reserve 100 bytes for the array
     ;----------------------------------------------------------------------------------------------
-
-
-    
