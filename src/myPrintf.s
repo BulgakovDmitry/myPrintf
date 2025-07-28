4e46dbad416240b741a36aa2c8b6febd84fe57fa
BulgakovDmitry <dima.olympiad11@yandex.ru>
2025-07-28 04:56:45 +0300

    wrote the basic logic of the main cycle

--- src/myPrintf.s
+++ src/myPrintf.s
@@ -37,3 +37,35 @@ exit:                                            ; точка выхода
     add  rsp, 48                                 ; значения регистров уже не нужны => снимаем их 48 = 8 байт * 6 регистров
     ret                                          
 
+;==================================================================================================
+; ___________________________parse_________________________________________________________________  
+; DESCRIPTION: main cycle in which buffer parsing takes place
+; ENTRY:       rsp
+; EXIT:        none
+; DESTROY:     rax, rbx, rcx, rdx 
+;==================================================================================================
+parse:
+    mov  rax, [rsp]                              ; положим в rax адрес форматой строки (текущая вершина стека)
+    lea  rbx, [rsp + 8]                          ; положим в rsp адрес первого аргумента myPrintf
+    xor  rcx, rcx                                ; обнуление rcx
+    mov  byte [bufferPosition], 0                ; инициализация позиции в буфере (стартовая позиция 0)
+
+mainCycle:
+    xor     rdx, rdx                             ; обнуляем rdx
+    mov     dl,  [rax]                           ; кладем текущую позицию форматной строки в младший байт rdx
+
+    test  dl, dl                                 ; if (*fmt == '\0')
+    jz    mainCycleEnd                           ;     goto mainCycleEnd
+
+    cmp   dl, '%'                                ; if (strcmp(*fmt, '%') == 0
+    je    specificator                           ;     goto specificator
+
+    cmp dl, '$'                                  ; if (strcmp(*fmt, '%') == 0
+    je    colorSpecificator                      ;     goto colorCpecificator
+
+    ;                                            ; обработка "обычного" символа (не конец строки и не спецификатор)
+    mov   dl, byte [rax]                         ; положим текущий символ в младший байт rdx
+    call  myPutc                                 ; выведем этот символ
+    inc   rax                                    ; fmt++
+    inc   rcx                                    ; счётчик++
+    jmp   mainCycle                              ; возобновляем цикл для следующего символа
